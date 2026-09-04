c042d95e92d61ef62e83d64f00f41f9ec5c378c9
Ryan Hellard <ryanhellard1@gmail.com>
2026-09-03 22:32:36 -0400

    Use deterministic D3 pair RNG and fail closed on invalid probabilities

--- sidm/sidm_core_flux_computation.h
+++ sidm/sidm_core_flux_computation.h
@@ -27,44 +27,66 @@
         }
 #endif
         if(prob > 0.2) {out.dtime_sidm = DMIN(out.dtime_sidm , local.dtime*(0.2/prob));} // timestep condition not being met as desired, warn code to lower timestep next turn //
-        if (gsl_rng_uniform(random_generator) < prob)
+#ifndef GRAIN_COLLISIONS
+        if(d3_mode > 0 && prob > 1.0)
         {
-#ifdef WAKEUP
-            if(!(TimeBinActive[P[j].TimeBin])) {if(WAKEUP*local.dtime < Pj_dtime) {
-                #pragma omp atomic write
-                PPPZ[j].wakeup=1;
-                #pragma omp atomic write
-                NeedToWakeupParticles_local = 1;
-            }}
+            if(ThisTask == 0) fprintf(stderr,"SIDMx-D3: fatal pair probability %g > 1 at integer time %llu\n",prob,(unsigned long long)All.Ti_Current);
+            endrun(171204);
+        }
 #endif
+        {
+            double sidmx_accept_draw;
 #ifndef GRAIN_COLLISIONS
             if(d3_mode > 0)
-            {
-                double delta_i[3], delta_j[3];
-                sidmx_d3_scatter_deltas(d3_mode,d3_channel,kernel.dv,local.Mass,P[j].Mass,delta_i,delta_j);
-                int k; for(k=0;k<3;k++) {
-                    out.sidm_kick[k] += delta_i[k];
-                    /* Keep the local working velocity synchronized so another
-                     * accepted collision in this neighbor walk sees the updated
-                     * state, not the pre-collision velocity. */
-                    local.Vel[k] += delta_i[k];
-                    #pragma omp atomic
-                    P[j].Vel[k] += delta_j[k];
-                }
-            }
+                sidmx_accept_draw = sidmx_d3_pair_uniform((unsigned long long)local.ID,(unsigned long long)P[j].ID,
+                                                           (unsigned long long)All.Ti_Current,d3_mode,0);
             else
 #endif
+                sidmx_accept_draw = gsl_rng_uniform(random_generator);
+
+            if (sidmx_accept_draw < prob)
             {
-                double kick[3]; calculate_interact_kick(kernel.dv, kick, m_si);
-                int k; for(k=0;k<3;k++) {
-                    out.sidm_kick[k] -= (P[j].Mass/m_si)*kick[k];
-                    #pragma omp atomic
-                    P[j].Vel[k] += (local.Mass/m_si)*kick[k]; // this variable is modified here so need to do this carefully here to ensure we don't multiply-write at the same time
+#ifdef WAKEUP
+                if(!(TimeBinActive[P[j].TimeBin])) {if(WAKEUP*local.dtime < Pj_dtime) {
+                    #pragma omp atomic write
+                    PPPZ[j].wakeup=1;
+                    #pragma omp atomic write
+                    NeedToWakeupParticles_local = 1;
+                }}
+#endif
+#ifndef GRAIN_COLLISIONS
+                if(d3_mode > 0)
+                {
+                    double delta_i[3], delta_j[3];
+                    double u_mu = sidmx_d3_pair_uniform((unsigned long long)local.ID,(unsigned long long)P[j].ID,
+                                                        (unsigned long long)All.Ti_Current,d3_mode,1);
+                    double u_phi = sidmx_d3_pair_uniform((unsigned long long)local.ID,(unsigned long long)P[j].ID,
+                                                         (unsigned long long)All.Ti_Current,d3_mode,2);
+                    sidmx_d3_scatter_deltas(d3_mode,d3_channel,kernel.dv,local.Mass,P[j].Mass,u_mu,u_phi,delta_i,delta_j);
+                    int k; for(k=0;k<3;k++) {
+                        out.sidm_kick[k] += delta_i[k];
+                        /* Keep the local working velocity synchronized so another
+                         * accepted collision in this neighbor walk sees the updated
+                         * state, not the pre-collision velocity. */
+                        local.Vel[k] += delta_i[k];
+                        #pragma omp atomic
+                        P[j].Vel[k] += delta_j[k];
+                    }
                 }
+                else
+#endif
+                {
+                    double kick[3]; calculate_interact_kick(kernel.dv, kick, m_si);
+                    int k; for(k=0;k<3;k++) {
+                        out.sidm_kick[k] -= (P[j].Mass/m_si)*kick[k];
+                        #pragma omp atomic
+                        P[j].Vel[k] += (local.Mass/m_si)*kick[k]; // this variable is modified here so need to do this carefully here to ensure we don't multiply-write at the same time
+                    }
+                }
+                out.si_count++;
+                #pragma omp atomic
+                P[j].NInteractions++;
             }
-            out.si_count++;
-            #pragma omp atomic
-            P[j].NInteractions++;
         }
     } // if((1 << ptype) & (DM_SIDM))
 }
