6680d89cb41eadcf1fc87db5fc13202415bbd6dc
Ryan Hellard <ryanhellard1@gmail.com>
2026-09-03 22:21:12 -0400

    Wire D3 channel probability and unequal-mass scattering into GIZMO SIDM neighbor loop

--- sidm/sidm_core_flux_computation.h
+++ sidm/sidm_core_flux_computation.h
@@ -1,5 +1,6 @@
 /* here is where we call the core of the SIDM calculation for DM particle-particle interactions */
 #ifdef DM_SIDM
+#include "sidmx_d3.h"
 {
     /* check if target+neighbor are an SIDM candidate, and against self-interaction */
     double Pj_dtime = GET_PARTICLE_TIMESTEP_IN_PHYSICAL(j);
@@ -7,10 +8,23 @@
     {
         if((local.dtime==Pj_dtime) && (local.ID > P[j].ID)) continue; // ensures interaction will only be calculated once for each pair //
         double h_si = 0.5*(kernel.h_i + kernel.h_j), m_si = 0.5*(local.Mass + P[j].Mass);
+        double prob = 0.0;
+        int d3_mode = 0, d3_channel = -1;
 #ifdef GRAIN_COLLISIONS
-        double prob = prob_of_grain_interaction(local.Grain_CrossSection_PerUnitMass , local.Mass, kernel.r, h_si, kernel.dv, local.dtime, j);
+        prob = prob_of_grain_interaction(local.Grain_CrossSection_PerUnitMass , local.Mass, kernel.r, h_si, kernel.dv, local.dtime, j);
 #else
-        double prob = prob_of_interaction(m_si, kernel.r, h_si, kernel.dv, local.dtime);
+        d3_mode = sidmx_d3_runtime_mode();
+        if(d3_mode > 0)
+        {
+            d3_channel = sidmx_d3_channel(local.Type,P[j].Type);
+            if(d3_channel >= 0)
+                prob = sidmx_d3_probability(d3_mode,d3_channel,local.Type,P[j].Type,
+                                             local.Mass,P[j].Mass,kernel.r,h_si,kernel.dv,local.dtime);
+        }
+        else
+        {
+            prob = prob_of_interaction(m_si, kernel.r, h_si, kernel.dv, local.dtime);
+        }
 #endif
         if(prob > 0.2) {out.dtime_sidm = DMIN(out.dtime_sidm , local.dtime*(0.2/prob));} // timestep condition not being met as desired, warn code to lower timestep next turn //
         if (gsl_rng_uniform(random_generator) < prob)
@@ -23,11 +37,30 @@
                 NeedToWakeupParticles_local = 1;
             }}
 #endif
-            double kick[3]; calculate_interact_kick(kernel.dv, kick, m_si);
-            int k; for(k=0;k<3;k++) {
-                out.sidm_kick[k] -= (P[j].Mass/m_si)*kick[k];
-                #pragma omp atomic
-                P[j].Vel[k] += (local.Mass/m_si)*kick[k]; // this variable is modified here so need to do this carefully here to ensure we don't multiply-write at the same time
+#ifndef GRAIN_COLLISIONS
+            if(d3_mode > 0)
+            {
+                double delta_i[3], delta_j[3];
+                sidmx_d3_scatter_deltas(d3_mode,d3_channel,kernel.dv,local.Mass,P[j].Mass,delta_i,delta_j);
+                int k; for(k=0;k<3;k++) {
+                    out.sidm_kick[k] += delta_i[k];
+                    /* Keep the local working velocity synchronized so another
+                     * accepted collision in this neighbor walk sees the updated
+                     * state, not the pre-collision velocity. */
+                    local.Vel[k] += delta_i[k];
+                    #pragma omp atomic
+                    P[j].Vel[k] += delta_j[k];
+                }
+            }
+            else
+#endif
+            {
+                double kick[3]; calculate_interact_kick(kernel.dv, kick, m_si);
+                int k; for(k=0;k<3;k++) {
+                    out.sidm_kick[k] -= (P[j].Mass/m_si)*kick[k];
+                    #pragma omp atomic
+                    P[j].Vel[k] += (local.Mass/m_si)*kick[k]; // this variable is modified here so need to do this carefully here to ensure we don't multiply-write at the same time
+                }
             }
             out.si_count++;
             #pragma omp atomic
